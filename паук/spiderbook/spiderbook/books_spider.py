72bef1f18c196d2c2016d2a8e21adeceda53e18f
Тимур <165512910+Oddenweird@users.noreply.github.com>
2024-12-07 19:54:41 +0300

    Update books_spider.py

--- паук/spiderbook/spiderbook/books_spider.py
+++ паук/spiderbook/spiderbook/books_spider.py
@@ -9,16 +9,13 @@ class BooksSpider(scrapy.Spider):
         'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
     }
    
-    def parse(self, response):
-        # Извлечение названий книг на текущей странице
+    def parse(self, response):        
         book_titles = response.xpath('//h3/a/@title').getall()
         
         for title in book_titles:
             yield {'title': title}
             print(title)
-
       
-        # Пагинация: проверяем наличие следующей страницы
         next_page = response.xpath('//li[@class="next"]/a/@href').get()
         if next_page:
             yield response.follow(next_page, self.parse)
